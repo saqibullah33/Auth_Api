607c71a671ebb3af0bc86e1dafe2bb90fa1f65c3
SAQIB ULLAH <108968870+saqibullah33@users.noreply.github.com>
2023-09-14 08:19:31 +0500

    Update otp.js

--- helper/otp.js
+++ helper/otp.js
@@ -1,7 +1,6 @@
 function generateOTP() {
           
-    // Declare a digits variable 
-    // which stores all digits
+    
     var digits = '0123456789';
     let OTP = '';
     for (let i = 0; i < 4; i++ ) {
